71a917fc7da2c699760f0376cad0f7ee221ca887
Frederick Ribolzi <frederickribolzi@gmail.com>
2023-05-18 10:24:46 -0500

    Damage Start

---
+++ Assets/Scripts/ScriptableObjects/DamageVariable.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Damage Variable")]
+public class Damage : ScriptableObject
+{
+    public int damage;
+}
