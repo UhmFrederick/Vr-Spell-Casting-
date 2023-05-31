71cae3337403c533d866b0a1be7ef35461aa35cf
Frederick Ribolzi <frederickribolzi@gmail.com>
2023-05-31 17:41:15 -0500

    fireball

--- Assets/Scripts/Spells/FireBall/FireBall.cs
+++ Assets/Scripts/Spells/FireBall/FireBall.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class FireBall : MonoBehaviour
 {
     private Rigidbody Rigidbody;
+    float speed;
     // Start is called before the first frame update
     void Start()
     {
         Rigidbody = this.GetComponent<Rigidbody>();
-        Rigidbody.AddRelativeForce(new Vector3());
+        Rigidbody.AddRelativeForce(Vector3.forward * speed);
     }
 
     // Update is called once per frame
